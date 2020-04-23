c68d39ed5fd559f212599c9812979fcf8fee091b
Fallasin <dian.yordanov.z@gmail.com>
2020-04-23 14:53:30 +0300

    updating easygit.js in order to add new options

--- libEasyGit/functions.js
+++ libEasyGit/functions.js
@@ -2114,15 +2114,20 @@ function splitString(str) {
     return s1;
     };
 
-function validURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-    return !!pattern.test(str);
-    }
+// function validURL(str) {
+//     var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+//         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+//         '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+//         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+//         '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+//         '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+//     return !!pattern.test(str);
+//     }
+
+function validURL(string) {
+    var res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    return (res !== null)
+}
 
 module.exports = {
     limitStringLinesCount:limitStringLinesCount, 
